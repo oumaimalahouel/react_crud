7143e3e8c9e47590d7afb0e04c207e8631400ebb
oumaima <lahoueloumaima1@gmail.com>
2023-04-11 05:00:12 +0100

    Second commit

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./component/Home";
+import "./index.css";
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
+
+
+
